deb5564cf37dd8d9d1707612768f7daf4bc2a4a6
Adi <oradam86@gmail.com>
2022-07-20 16:33:18 +0200

    Base setup almost ready

---
+++ lib/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calc',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SafeArea(
+        child: MyHomePage(title: 'BMI Calculator'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text("Body Text"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
